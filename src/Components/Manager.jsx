0140a118a2d096a5b282fd31a1fef659ea465d6d
ORavid25 <oravid12@gmail.com>
2021-10-09 18:10:49 +0300

    first commit

---
+++ src/Components/Manager.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import {
+  withRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Home from "../Screens/Home";
+import Login from "../Screens/Login";
+
+const Manager = () => {
+  return (
+    <div>
+    <Switch>
+      {/* If the current URL is /about, this route is rendered
+          while the rest are ignored */}
+      <Route path="/">
+        <Login />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+    </Switch>
+  </div>
+  );
+};
+
+export default withRouter(Manager);
