a03fbe7f7c279048d5be37c676506518fe2d9e67
Shaked5 <shakedshervi@gmail.com>
2021-10-27 21:13:24 +0300

    ..

--- src/Components/ItemsDisplay.jsx
+++ src/Components/ItemsDisplay.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const ItemsDisplay = () => {
+const ItemsDisplay = ({ businessItems }) => {
+
+  useEffect(() => {
+    
+    const res = businessItems.filter(item => item.itemID == 12)
+    console.log("filter", res);
+  }, [businessItems]);
+
+  
   return (
     <div dir="rtl" className="w-full h-full rounded-xl bg-white">
       <div className="flex flex-col justify-around p-5 bg-gray-200 h-20 rounded-t-xl">
@@ -8,9 +16,17 @@ const ItemsDisplay = () => {
         <h2 className="text-md font-normal text-gray-500">כאן תוכלו לראות את רשימת המוצרים שלכם</h2>
       </div>
 
+      {businessItems.map((item, index) => {
+        return (
+          <div key={index} className="">{item.itemID}{item.toppingName}</div>
+        )
+      })}
+
+
       <div className="bg-green-200 w-full h-5/6">
 
       </div>
+
     </div>
   );
 };
