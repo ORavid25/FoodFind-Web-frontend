247a43bc6922aebfce765e6b2435220fef590836
ORavid25 <oravid12@gmail.com>
2021-10-27 21:19:48 +0300

    ///

--- src/Components/ItemsDisplay.jsx
+++ src/Components/ItemsDisplay.jsx
@@ -1,89 +1,69 @@
 import React, { useEffect } from "react";
 
-<<<<<<< HEAD
 const Item = () => {
-    return(
-        <div className=" w-full h-20 flex justify-center items-center bg-yellow-500 ">
-            <h1>itemName</h1>
+  return (
+    <div className=" w-full h-20 flex justify-center items-center bg-yellow-500 ">
+      <h1>itemName</h1>
+    </div>
+  );
+};
 
-        </div>
-    )
-  }
-
-const ItemsDisplay = () => {
-
- 
-
-
-
-=======
 const ItemsDisplay = ({ businessItems }) => {
-
   useEffect(() => {
-    
-    const res = businessItems.filter(item => item.itemID == 12)
+    const res = businessItems.filter((item) => item.itemID == 12);
     console.log("filter", res);
   }, [businessItems]);
 
-  
->>>>>>> e3a43d70ccca2a430b8d760d40d0ba711541d904
   return (
     <div dir="rtl" className="w-full h-5/6 rounded-xl   px-5 py-5">
       <div className="flex flex-col justify-around p-5 bg-gray-200 h-20 rounded-t-xl">
         <h1 className="text-lg font-medium my-2">רשימת המוצרים</h1>
-        <h2 className="text-md font-normal text-gray-500">כאן תוכלו לראות את רשימת המוצרים שלכם</h2>
+        <h2 className="text-md font-normal text-gray-500">
+          כאן תוכלו לראות את רשימת המוצרים שלכם
+        </h2>
       </div>
 
-<<<<<<< HEAD
-        <div className="grid grid-cols-2 gap-x-20 gap-y-16 h-5/6 p-5 rounded-b-lg bg-green-400  overflow-y-scroll">
+      <div className="grid grid-cols-2 gap-x-20 gap-y-16 h-5/6 p-5 rounded-b-lg bg-green-400  overflow-y-scroll">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
 
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
 
-          
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-          <Item/>
-   
-
-      
-        </div>
-      
-=======
       {businessItems.map((item, index) => {
         return (
-          <div key={index} className="">{item.itemID}{item.toppingName}</div>
-        )
+          <div key={index} className="">
+            {item.itemID}
+            {item.toppingName}
+          </div>
+        );
       })}
 
-
-      <div className="bg-green-200 w-full h-5/6">
-
-      </div>
-
->>>>>>> e3a43d70ccca2a430b8d760d40d0ba711541d904
+      <div className="bg-green-200 w-full h-5/6"></div>
     </div>
   );
 };
