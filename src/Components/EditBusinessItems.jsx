5cb8dfa3c7f091bd6b2f4a939cb827ed892a3e18
Shaked5 <shakedshervi@gmail.com>
2021-11-01 16:12:25 +0200

    ....

--- src/Components/EditBusinessItems.jsx
+++ src/Components/EditBusinessItems.jsx
@@ -23,9 +23,7 @@ const EditBusinessItems = ({ ItemForEdit, itemToppings }) => {
       const activeTopping = itemToppings.filter(item => item.isActive === true)
       await setActiveToppings(activeTopping);
     })()
-
-
-  }, []);
+  }, [itemToppings]);
 
   const handleUpdateBusinessItem = async () => {
     debugger;
@@ -44,17 +42,17 @@ const EditBusinessItems = ({ ItemForEdit, itemToppings }) => {
   };
 
   return (
-    <div className="flex flex-col justify-around w-11/12 p-5 h-max rounded-t-xl ">
-      <div className="rounded-t-xl bg-gray-300 p-5 shadow-xl">
+    <div className="flex flex-col justify-around w-11/12 p-5 max-h-full rounded-t-xl ">
+      <div className="rounded-t-xl bg-gray-300 rounded shadow-xl p-5">
         <h1 className="text-lg font-medium ">פירוט המוצרים והתוספות</h1>
         <h2 className="text-md font-normal text-gray-500">
           כאן ניתן לבצע שינויים במוצר ובתוספות המוצר
         </h2>
       </div>
       <div className=" flex justify-center items-center p-2 bg-gray-300">
-        <h1 className="text-xl font-semibold">{ItemForEdit.itemName}</h1>
+        <h1 className="text-md font-semibold">{ItemForEdit.itemName}</h1>
       </div>
-      <div className="flex h-64 justify-evenly items-center flex-row p-5 font-semibold text-lg  bg-gray-300 ">
+      <div className="flex max-h-40 justify-evenly items-center flex-row font-semibold text-lg  bg-gray-300 ">
         <h1>מחיר מוצר</h1>
         <input
           type="number"
