07a51ef15bb4416f8641e36eed5a09317121eab5
ORavid25 <oravid12@gmail.com>
2021-12-07 22:11:05 +0200

    ...

--- src/Screens/BusinessReport.jsx
+++ src/Screens/BusinessReport.jsx
@@ -7,48 +7,11 @@ const BusinessReport = () => {
     <Layout>
       <Navbar />
 
-      <div className="ml-44 bg-blue-400 h-screen">
-      <div className="bg-pink-600 pt-3">
-          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error quos ratione debitis. Minima eius porro voluptatem sit eum earum aliquam. Facere fugit quae voluptatibus aperiam delectus a ut laboriosam?
-          Ab explicabo aperiam odio dignissimos corporis commodi voluptas adipisci necessitatibus provident, asperiores voluptatibus repellat quos rerum. Officiis alias asperiores voluptatibus repellat voluptatum, libero, enim error non expedita excepturi, vero doloremque.
-          Provident maiores modi a. Pariatur iste nesciunt sunt illo velit. Dolores veritatis, ea officia impedit esse placeat molestiae consectetur corporis nobis tempora perferendis mollitia atque reprehenderit dicta, dolorem quod sed.
-          Ipsum, expedita quas. Eum modi corrupti in cumque pariatur. Officiis quam fugiat temporibus aliquam non. Obcaecati nobis quis velit laudantium ullam quisquam ipsum quas totam aut, nam est sit doloribus.
-          Ratione aut nulla possimus ullam. Alias, fuga! Ea sapiente recusandae quidem deserunt eos architecto unde magnam inventore excepturi maxime ab explicabo delectus sint quasi perspiciatis quos, soluta aut amet ex?</h1>
+      <div className="ml-44  bg-blue-400 h-screen">
+        <div className="bg-red-500 w-full h-36">
+
         </div>
-        <div className="bg-indigo-200 pt-3">
-          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error quos ratione debitis. Minima eius porro voluptatem sit eum earum aliquam. Facere fugit quae voluptatibus aperiam delectus a ut laboriosam?
-          Ab explicabo aperiam odio dignissimos corporis commodi voluptas adipisci necessitatibus provident, asperiores voluptatibus repellat quos rerum. Officiis alias asperiores voluptatibus repellat voluptatum, libero, enim error non expedita excepturi, vero doloremque.
-          Provident maiores modi a. Pariatur iste nesciunt sunt illo velit. Dolores veritatis, ea officia impedit esse placeat molestiae consectetur corporis nobis tempora perferendis mollitia atque reprehenderit dicta, dolorem quod sed.
-          Ipsum, expedita quas. Eum modi corrupti in cumque pariatur. Officiis quam fugiat temporibus aliquam non. Obcaecati nobis quis velit laudantium ullam quisquam ipsum quas totam aut, nam est sit doloribus.
-          Ratione aut nulla possimus ullam. Alias, fuga! Ea sapiente recusandae quidem deserunt eos architecto unde magnam inventore excepturi maxime ab explicabo delectus sint quasi perspiciatis quos, soluta aut amet ex?</h1>
-        </div>
-        <div className="bg-yellow-700 pt-3">
-          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error quos ratione debitis. Minima eius porro voluptatem sit eum earum aliquam. Facere fugit quae voluptatibus aperiam delectus a ut laboriosam?
-          Ab explicabo aperiam odio dignissimos corporis commodi voluptas adipisci necessitatibus provident, asperiores voluptatibus repellat quos rerum. Officiis alias asperiores voluptatibus repellat voluptatum, libero, enim error non expedita excepturi, vero doloremque.
-          Provident maiores modi a. Pariatur iste nesciunt sunt illo velit. Dolores veritatis, ea officia impedit esse placeat molestiae consectetur corporis nobis tempora perferendis mollitia atque reprehenderit dicta, dolorem quod sed.
-          Ipsum, expedita quas. Eum modi corrupti in cumque pariatur. Officiis quam fugiat temporibus aliquam non. Obcaecati nobis quis velit laudantium ullam quisquam ipsum quas totam aut, nam est sit doloribus.
-          Ratione aut nulla possimus ullam. Alias, fuga! Ea sapiente recusandae quidem deserunt eos architecto unde magnam inventore excepturi maxime ab explicabo delectus sint quasi perspiciatis quos, soluta aut amet ex?</h1>
-        </div>        <div className="bg-pink-600 pt-3">
-          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error quos ratione debitis. Minima eius porro voluptatem sit eum earum aliquam. Facere fugit quae voluptatibus aperiam delectus a ut laboriosam?
-          Ab explicabo aperiam odio dignissimos corporis commodi voluptas adipisci necessitatibus provident, asperiores voluptatibus repellat quos rerum. Officiis alias asperiores voluptatibus repellat voluptatum, libero, enim error non expedita excepturi, vero doloremque.
-          Provident maiores modi a. Pariatur iste nesciunt sunt illo velit. Dolores veritatis, ea officia impedit esse placeat molestiae consectetur corporis nobis tempora perferendis mollitia atque reprehenderit dicta, dolorem quod sed.
-          Ipsum, expedita quas. Eum modi corrupti in cumque pariatur. Officiis quam fugiat temporibus aliquam non. Obcaecati nobis quis velit laudantium ullam quisquam ipsum quas totam aut, nam est sit doloribus.
-          Ratione aut nulla possimus ullam. Alias, fuga! Ea sapiente recusandae quidem deserunt eos architecto unde magnam inventore excepturi maxime ab explicabo delectus sint quasi perspiciatis quos, soluta aut amet ex?</h1>
-        </div>
-        <div className="bg-indigo-200 pt-3">
-          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error quos ratione debitis. Minima eius porro voluptatem sit eum earum aliquam. Facere fugit quae voluptatibus aperiam delectus a ut laboriosam?
-          Ab explicabo aperiam odio dignissimos corporis commodi voluptas adipisci necessitatibus provident, asperiores voluptatibus repellat quos rerum. Officiis alias asperiores voluptatibus repellat voluptatum, libero, enim error non expedita excepturi, vero doloremque.
-          Provident maiores modi a. Pariatur iste nesciunt sunt illo velit. Dolores veritatis, ea officia impedit esse placeat molestiae consectetur corporis nobis tempora perferendis mollitia atque reprehenderit dicta, dolorem quod sed.
-          Ipsum, expedita quas. Eum modi corrupti in cumque pariatur. Officiis quam fugiat temporibus aliquam non. Obcaecati nobis quis velit laudantium ullam quisquam ipsum quas totam aut, nam est sit doloribus.
-          Ratione aut nulla possimus ullam. Alias, fuga! Ea sapiente recusandae quidem deserunt eos architecto unde magnam inventore excepturi maxime ab explicabo delectus sint quasi perspiciatis quos, soluta aut amet ex?</h1>
-        </div>
-        <div className="bg-yellow-700 pt-3">
-          <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore error quos ratione debitis. Minima eius porro voluptatem sit eum earum aliquam. Facere fugit quae voluptatibus aperiam delectus a ut laboriosam?
-          Ab explicabo aperiam odio dignissimos corporis commodi voluptas adipisci necessitatibus provident, asperiores voluptatibus repellat quos rerum. Officiis alias asperiores voluptatibus repellat voluptatum, libero, enim error non expedita excepturi, vero doloremque.
-          Provident maiores modi a. Pariatur iste nesciunt sunt illo velit. Dolores veritatis, ea officia impedit esse placeat molestiae consectetur corporis nobis tempora perferendis mollitia atque reprehenderit dicta, dolorem quod sed.
-          Ipsum, expedita quas. Eum modi corrupti in cumque pariatur. Officiis quam fugiat temporibus aliquam non. Obcaecati nobis quis velit laudantium ullam quisquam ipsum quas totam aut, nam est sit doloribus.
-          Ratione aut nulla possimus ullam. Alias, fuga! Ea sapiente recusandae quidem deserunt eos architecto unde magnam inventore excepturi maxime ab explicabo delectus sint quasi perspiciatis quos, soluta aut amet ex?</h1>
-        </div>
+     
       </div>
     </Layout>
   );
